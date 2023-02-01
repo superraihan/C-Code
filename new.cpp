6b3f1463ccbe5e3308c529eddbd16bd3830c1f0a
MD.RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-01 14:42:40 +0000

    first commit

---
+++ new.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+    int first_number = 20;
+    int secound_number = 10;
+    int sum;
+    sum = first_number + secound_number;
+    cout << "summation";
+    cout << sum;
+    return 0;
+
+}
