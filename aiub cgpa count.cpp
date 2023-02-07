72dcc7768bd9be3c68d0d2562cf426fac7aab985
MD.RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-07 23:18:23 +0600

    Add files via upload

---
+++ aiub cgpa count.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int x;
+cin>>x;
+if(x=90 && x<=100)
+
+{
+   cout<<"A+";
+
+}
+
+int x;
+cin>>x;
+if(x=85 && x<=90)
+{
+    cout<<"A";
+
+}
+
+int x;
+cin>>x;
+if(x=80 && x<=85)
+{
+    cout<<"B+";
+
+}
+
+int x;
+cin>>x;
+if(x=75 && x=80)
+{
+    cout<<"B";
+}
+
+int x;
+cin>>x;
+if(x=70 && x=75)
+{
+    cout<<"C+";
+
+}
+
+int x;
+cin>>x;
+if(x=65 && x=70)
+{
+    cout<<"C";
+}
+
+int x;
+cin>>x;
+if(x=60 && x=65)
+{
+    cout<<"D+";
+
+}
+ int x;
+ cin>>x;
+ if(x=50 && x=60)
+ {
+     cout<<"D";
+ }
+
+int x;
+cin>>x;
+if(x=40 && x=50)
+{
+    cout<<"UW";
+}
+
+}
+
+
