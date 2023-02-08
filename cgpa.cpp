03f631ef3c9e57891df757802637c152fc378053
MD.RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-08 18:38:01 +0600

    Add files via upload

---
+++ cgpa.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int x;
+
+
+cin>> x;
+if(x>=90)
+{
+   cout<<"A+";
+
+}
+ else if (x>=85 && x<=90)
+{
+    cout<<"A";
+
+}
+
+
+ else if (x>=80 && x<=85)
+{
+    cout<<"B+";
+
+}
+
+ else if (x>=75 && x<=80)
+{
+    cout<<"B";
+}
+
+ else if (x>=70 && x<=75)
+{
+    cout<<"C+";
+
+}
+
+ else if (x>=65  && x<=70 )
+{
+    cout<<"C";
+}
+ else if  (x>=60 && x<=65)
+{
+    cout<<"D+";
+}
+  else if (x>=55 && x<=60 )
+ {
+     cout<<"D";
+ }
+else
+
+
+    cout<<"UW";
+
+}
+
