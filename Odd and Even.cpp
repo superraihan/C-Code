1a945d2100958d5116bcc155cda84735d3c4600e
MD.RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-09 18:22:46 +0600

    Add files via upload

---
+++ Odd and Even.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+  int number;
+  cout<<"inter your number ";
+  cin>>number;
+  if(number % 2== 0)
+    cout << number << " is even ";
+ else
+    cout << number << " is odd ";
+
+
+  number = -number;
+
+  return 0;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
