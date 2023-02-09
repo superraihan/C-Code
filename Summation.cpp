1332ef50f519cf5baa771a26ba954e378204b603
MD.RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-09 18:33:49 +0600

    Add files via upload

---
+++ Summation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+    int first_number = 20;
+    int secound_number = 10;
+    int sum;
+    cout << " first number? ";
+    cin >> first_number;
+    cout << " second number? ";
+    cin >> secound_number;
+    sum = first_number + secound_number;
+    cout <<"summation of first and second number";
+    cout << sum;
+    return 0;
+
+
+}
+
