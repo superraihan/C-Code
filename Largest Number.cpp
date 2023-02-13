72fcd8290971dd23f574b96a8a5c25567de06a7b
MD.RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-13 19:47:12 +0600

    Add files via upload

---
+++ Largest Number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+
+ int x,y,z;
+
+ cout<<" Enter Your Number "<<endl;
+ cin>>x>>y>>z;
+
+ if(x>y && y>>z)
+ {
+     cout<<" Largest Number = "<<y;
+
+ }
+
+  if(z>x && y<z)
+  {
+      cout<<" Largest Number = "<<z;
+
+  }
+
+ else
+ {
+     cout<<" Equal ";
+
+ }
+   return 0;
+
+
+}
