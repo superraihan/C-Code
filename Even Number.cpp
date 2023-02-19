1fd63733ed54329c153e4d5c48b34f16f14abfbe
MD. RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-02-19 19:29:12 +0600

    Add files via upload

--- Even Number.cpp
+++ Even Number.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Even numbers from 2 to " << num << ":" << endl;
+    for (int i = 2; i <= num; i += 2) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
 
