43ae0c3c8adc8804aa204c09bcd1ff3ac32f4696
MD. RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-03-19 13:12:57 +0600

    Add files via upload

---
+++ 2D Array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+  int A [2][3];
+
+  A[0][0] = 10;
+  A[0][1] = 20;
+  A[0][2] = 30;
+
+  A[1][0] = 40;
+  A[1][1] = 50;
+  A[1][2] = 60;
+
+
+for(int row=0; row<2; row++)
+{
+
+    for(int col=0; col<3; col++)
+     {
+         cout<< A[row][col]<<" ";
+     }
+     cout<<endl;
+
+}
+getch();
+
+
+}
