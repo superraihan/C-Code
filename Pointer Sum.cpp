13dd4a962991929bb3ba173c16101ecc8f390950
MD. RAIHAN KABIR NIBIR <124171192+superraihan@users.noreply.github.com>
2023-04-01 11:40:03 +0600

    Add files via upload

---
+++ Pointer Sum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num1 = 10;
+ int num2 = 20;
+
+ int *p1 , *p2;
+
+ p1 =& num1;
+ p2 =& num2;
+
+ int sum = *p1 + *p2;
+
+ cout<<sum<<endl;
+
+return 0;
+
+
+
+
+}
+
